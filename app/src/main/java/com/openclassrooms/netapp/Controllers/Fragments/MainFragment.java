1bb61e6dca5730b77b8ea6608366e8d674b3bd48
Anthony Mamode <amamode@appstud.com>
2018-07-31 19:24:04 +0200

    Finish chapter RxJava with simple strings

--- app/src/main/java/com/openclassrooms/netapp/Controllers/Fragments/MainFragment.java
+++ app/src/main/java/com/openclassrooms/netapp/Controllers/Fragments/MainFragment.java
@@ -4,6 +4,7 @@ package com.openclassrooms.netapp.Controllers.Fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +20,10 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+import io.reactivex.observers.DisposableObserver;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,6 +32,9 @@ public class MainFragment extends Fragment implements NetworkAsyncTask.Listeners
 
     // FOR DESIGN
     @BindView(R.id.fragment_main_textview) TextView textView;
+
+    //Declare subscription for RxJava
+    private Disposable disposable;
 
     public MainFragment() { }
 
@@ -43,7 +51,7 @@ public class MainFragment extends Fragment implements NetworkAsyncTask.Listeners
 
     @OnClick(R.id.fragment_main_button)
     public void submit(View view) {
-        this.executeHttpRequestWithRetrofit();
+        this.streamShowString();
     }
 
     @Override
@@ -87,5 +95,62 @@ public class MainFragment extends Fragment implements NetworkAsyncTask.Listeners
     }
     private void updateUIWhenStopingHTTPRequest(String s) {
         this.textView.setText(s);
+    }
+
+    // ------------------------------
+    //  Reactive X
+    // ------------------------------
+
+    // 1 - Create Observable
+    private Observable<String> getObservable(){
+        return Observable.just("Cool !");
+    }
+    // 2 - Create Subscriber
+    private DisposableObserver<String> getSubscriber(){
+        return new DisposableObserver<String>() {
+            @Override
+            public void onNext(String s) {
+                textView.setText("Observable emits : " +s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e("TAG", "On error"+Log.getStackTraceString(e));
+            }
+
+            @Override
+            public void onComplete() {
+                Log.e("TAG", "On Complete !!");
+            }
+        };
+    }
+    // 3 - Create Stream and execute it
+    private void streamShowString(){
+        this.disposable = this.getObservable()
+                .map(getFunctionUpperCase())
+                .flatMap(getSecondObservable())
+                .subscribeWith(getSubscriber());
+    }
+    // 4 - Dispose subscription --> Required in destroy fragment method to release resources
+    private void disposeWhenDestroy(){
+        if (this.disposable != null && !this.disposable.isDisposed())
+            this.disposable.dispose();
+    }
+
+    private Function<String, String> getFunctionUpperCase(){
+        return new Function<String, String>() {
+            @Override
+            public String apply(String s) throws Exception {
+                return s.toUpperCase();
+            }
+        };
+    }
+    private Function<String, Observable<String>> getSecondObservable(){
+        return new Function<String, Observable<String>>() {
+            @Override
+            public Observable<String> apply(String previousString) throws Exception {
+                return Observable.just(previousString + " I love OpenClassrooms !");
+            }
+        };
     }
 }
